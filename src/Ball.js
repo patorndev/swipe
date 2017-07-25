0dff3e71b6959d89394c3c21b1d94c815cdb5c70
patorndev <di_patorani@msn.com>
2017-07-25 16:10:29 +0700

    Deck component before React Native Elements

--- src/Ball.js
+++ src/Ball.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Animated } from 'react-native';
 
 class Ball extends Component {
+  componentWillMount() {
+    // Animated.ValueXY start(0,0)
+    this.position = new Animated.ValueXY(0, 0);
+    // Animated.spring is used to change value of element
+    Animated.spring(this.position, {
+      // Set explicitly as an object = finish: { x: 200, y: 500 }
+      toValue: { x: 200, y: 500 }
+    }).start();
+  }
+
   render() {
     return (
-      <View style={styles.ball} />
+      // Element inside Animated.View to modify position
+      // pasing ValueXY as prop
+      <Animated.View style={this.position.getLayout()}>
+        <View style={styles.ball} />
+      </Animated.View>
     );
   }
 }
